9cf54d46713e9f6623b611db6efb9ad1f796f466
davodius <davidhanebrant@gmail.com>
2024-06-25 22:17:20 +0200

    second

--- index.js
+++ index.js
@@ -3,4 +3,19 @@ console.log(x);
 
 console.log("Guess the number");
 
-console.readline;
+// Kom ihåg Code runner för play knapp
+// Simpel js server för webbläsare
+/* const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+}); */
